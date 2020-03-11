b06418abec0b771be99b7e6b80d419b413344df4
Keanu Ellwood <KEllwood@jhb.dvt.co.za>
2020-03-11 12:51:32 +0200

    add prompt

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ async function registerServiceWorker() {
     } else {
         console.warn('Service Worker is not supported');
     }
+
+    // if (window.location.protocol === 'http:') {
+    //     const requireHTTPS = document.getElementById('requireHTTPS');
+    //     const link = requireHTTPS.querySelector('a');
+    //     link.href = window.location.href.replace('http://', 'https://');
+    //     requireHTTPS.classList.remove('hidden');
+    //   }
 }
+
--- sw.js
+++ sw.js
@@ -1,51 +1,66 @@
-const cacheName = 'profile-page-v1';
-const staticAssets = ['./',
-'./index.js',
-'./index.html',
-'./manifest.webmanifest',
-'./app.js',
-'./css/card.css',
-'./css/footer.css',
-'./css/navigation.css',
-'./css/styling.css']
+const cacheName = "profile-page-v1";
+const staticAssets = [
+  "./",
+  "./index.js",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./app.js",
+  "./css/card.css",
+  "./css/footer.css",
+  "./css/navigation.css",
+  "./css/styling.css"
+];
 
-self.addEventListener('install', async event => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
+self.addEventListener("install", async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-    self.clients.claim();
+self.addEventListener("activate", event => {
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', async event => {
-    const req = event.request;
-    const url = new URL(req.url);
+self.addEventListener("fetch", async event => {
+  const req = event.request;
+  const url = new URL(req.url);
 
-    if (url.origin == location.origin) {
-        event.respondWith(cacheFirst(req));
-    }
-else{
+  if (url.origin == location.origin) {
+    event.respondWith(cacheFirst(req));
+  } else {
     event.respondWith(networkAndCache(req));
-}
+  }
 });
 
-async function cacheFirst(req){
-    const cache = await caches.open(cacheName);
+let deferredPrompt;
+
+self.addEventListener("beforeinstallprompt", function(e) {
+  e.preventDefault();
+  deferredPrompt = e;
+  showAddToHomeScreen();
+});
+
+function showAddToHomeScreen() {
+  var a2hsBtn = document.querySelector(".ad2hs-prompt");
+  a2hsBtn.style.display = "block";
+  a2hsBtn.addEventListener("click", addToHomeScreen);
+}
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch {
     const cached = await cache.match(req);
-    return cached || fetch(req);
-};
-
-async function networkAndCache(req){
-    const cache = await caches.open(cacheName);
-
-    try{
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
-    }catch{
-        const cached = await cache.match(req);
-        return cached;
-    }
+    return cached;
+  }
 }
